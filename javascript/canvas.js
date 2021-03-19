bb8f9dad27b877775f34d2579427913ec3918770
Tran Van <vanbaotran201194@gmail.com>
2021-03-19 12:57:32 +0100

    hangman.js done+canvas.js haft way done

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -2,26 +2,362 @@ class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
     // ... your code goes here
+    this.secretWord = hangman.secretWord;
   }
 
   createBoard() {
     // ... your code goes here
+    this.context = '';
+    this.context.drawLines()
   }
 
   drawLines() {
     // ... your code goes here
+    let x=450;
+    let y=750;
+   for (let i=0; i<this.secretWord.length;i++){
+      this.context.beginPath();
+      this.context.moveTo(x,y);
+      this.context.lineTo(x+=20,y)
+      this.context.stroke();
+      this.context.closePath();
+      x+=10;
+    }
   }
 
   writeCorrectLetter(letter) {
     // ... your code goes here
+    let x=455;
+    let y=740;
+    for (let i=0;i<this.secretWord.length;i++){
+      if (this.secretWord[i]===letter){ 
+        x=455+i*30;
+        this.context.font='40px serif'
+        this.context.fillText(letter,x,y);
+      }
+    }
+    hangman.addCorrectLetter(letter);
   }
 
   writeWrongLetter(letter, errorsLeft) {
     // ... your code goes here
+    let x = 700;
+    let y = 500;
+    hangman.addWrongLetter(letter);
+    for (let i=0; i<hangman.letters.length;i++){
+      if (hangman.checkClickedLetters(letter)){
+        y=500+i*30
+        this.context.font='40px serif';
+        this.context.fillText(letter,x,y)
+      }
+    }
   }
 
   drawHangman(errorsLeft) {
     // ... your code goes here
+    switch(hangman.errorsLeft){
+      case '8':
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case '7':
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,700);
+        this.context.lineTo(150,100);   
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case '6':
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,700);
+        this.context.lineTo(150,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,100);
+        this.context.lineTo(400,100);   
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case '5':
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,700);
+        this.context.lineTo(150,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,100);
+        this.context.lineTo(400,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,100);
+        this.context.lineTo(400,150);   
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case '4':
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,700);
+        this.context.lineTo(150,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,100);
+        this.context.lineTo(400,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,100);
+        this.context.lineTo(400,150);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.arc(400,190,40,0,Math.PI*2);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case '3':
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,700);
+        this.context.lineTo(150,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,100);
+        this.context.lineTo(400,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,100);
+        this.context.lineTo(400,150);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.arc(400,190,40,0,Math.PI*2);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,230);
+        this.context.lineTo(400,400);   
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case '2':
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,700);
+        this.context.lineTo(150,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,100);
+        this.context.lineTo(400,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,100);
+        this.context.lineTo(400,150);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.arc(400,190,40,0,Math.PI*2);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,230);
+        this.context.lineTo(400,400);   
+        this.context.stroke();
+        this.context.closePath();
+        
+        this.context.beginPath();
+        this.context.moveTo(400,400);
+        this.context.lineTo(350,450);   
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case '1':
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,700);
+        this.context.lineTo(150,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,100);
+        this.context.lineTo(400,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,100);
+        this.context.lineTo(400,150);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.arc(400,190,40,0,Math.PI*2);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,230);
+        this.context.lineTo(400,400);   
+        this.context.stroke();
+        this.context.closePath();
+        
+        this.context.beginPath();
+        this.context.moveTo(400,400);
+        this.context.lineTo(350,450);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,400);
+        this.context.lineTo(450,450);   
+        this.context.stroke();
+        this.context.closePath(); 
+        break;
+      case '0':
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,700);
+        this.context.lineTo(150,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(150,100);
+        this.context.lineTo(400,100);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,100);
+        this.context.lineTo(400,150);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.arc(400,190,40,0,Math.PI*2);
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,230);
+        this.context.lineTo(400,400);   
+        this.context.stroke();
+        this.context.closePath();
+        
+        this.context.beginPath();
+        this.context.moveTo(400,400);
+        this.context.lineTo(350,450);   
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.beginPath();
+        this.context.moveTo(400,400);
+        this.context.lineTo(450,450);   
+        this.context.stroke();
+        this.context.closePath();  
+
+        this.context.beginPath();
+        this.context.moveTo(350,270);
+        this.context.lineTo(450,270);   
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      default:
+        this.context.beginPath();
+        this.context.moveTo(100,750);
+        this.context.lineTo(150,700);
+        this.context.lineTo(200,750);
+        this.context.lineTo(100,750);
+        this.context.stroke();
+        this.context.closePath();
+    } 
+
   }
 
   gameOver() {
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,68 @@
 class Hangman {
-  constructor(words) {
+  constructor(words,secretWord,letters,guessedLetters,errorsLeft) {
     this.words = words;
+    this.secretWord=this.pickWord();
+    this.letters=[];
+    this.guessedLetters='';
+    this.errorsLeft=8;
     // ... your code goes here
   }
 
   pickWord() {
+    const index = Math.floor(Math.random()*this.words.length);
+    return this.words[index];
     // ... your code goes here
+
   }
 
-  checkIfLetter(keyCode) {
+  checkIfLetter(key) {
     // ... your code goes here
+    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    if (alphabet.indexOf(key)!==-1){
+      return true;
+    }
+    return false;
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    if (this.letters.indexOf(letter)===-1){
+      this.letters.push(letter);
+      return true;
+    }
+    return false;
+    
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+      this.guessedLetters+=letter;
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    if (this.secretWord.includes(letter)===false){
+      this.errorsLeft--; 
+    }
   }
 
   checkGameOver() {
     // ... your code goes here
+    if (this.errorsLeft===0){
+      return true;
+    }
+    return false;
+
   }
 
   checkWinner() {
     // ... your code goes here
+    const lettersInOrder = this.guessedLetters.split('').sort().join('');
+    const secretWordsInOrder = this.secretWord.split('').sort().join('');
+    if (lettersInOrder===secretWordsInOrder){
+      return true;
+    }
+    return false;
   }
 }
 
@@ -42,8 +75,8 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
 
     // ... your code goes here
   });
